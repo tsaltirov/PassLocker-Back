ad073d044a15de2c84d178d8e7da43ce90f6fe4f
Fernando <alfarogr@hotmail.com>
2024-05-23 18:39:27 +0200

    Remove seed module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './authentication/auth.module';
-import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
@@ -20,7 +19,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       }),
 
     AuthModule, 
-    SeedModule],
+  ],
   controllers: [],
   providers: [],
 })
