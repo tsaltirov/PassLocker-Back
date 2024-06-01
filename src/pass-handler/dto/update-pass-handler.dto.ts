9ddd7a28aadfb6336f47911c18c74e1d6fa28bdd
Fernando <alfarogr@hotmail.com>
2024-06-01 13:30:00 +0200

    Remove atributes in UpdatePassHandlerDto

--- src/pass-handler/dto/update-pass-handler.dto.ts
+++ src/pass-handler/dto/update-pass-handler.dto.ts
@@ -4,35 +4,5 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class UpdatePassHandlerDto extends PartialType(CreatePassHandlerDto) {
-
-    @ApiProperty({
-        example: 'ssssssddassd5645fsddfdgdgdgd',
-        description: 'Current id password',
-        format: 'string',
-        uniqueItems: true
-    })
-    @IsNotEmpty()
-    @IsString()
-    id: string;
-
-    @ApiProperty({
-        example: 'ssssssdsdadada33dassd5645fsddfdgdgdgd',
-        description: 'New password to set',
-        format: 'string',
-        
-    })
-    @IsNotEmpty()
-    @IsString()
-    password: string;
-
-    @IsOptional()
-    @IsString()
-    userName?: string;
-
-    @IsOptional()
-    @IsString()
-    userService?: string;
-
-
 }
 
